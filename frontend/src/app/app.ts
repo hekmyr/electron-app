2198a50821ea698f546a06ae4f85d4fd542deeeb
hekma <contact@hekma.dev>
2025-11-08 16:44:17 +0100

    chore(frontend): add and configure spartan ui

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,18 +1,83 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HlmBreadCrumbImports } from '@spartan-ng/helm/breadcrumb';
+import { HlmSidebarImports } from '@spartan-ng/helm/sidebar';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HlmBreadCrumbImports, HlmSidebarImports],
   template: `
-    <h1 class="font-bold text-xl">Welcome to {{ title() }}!</h1>
-
-    <p>{{ content() }}</p>
+    <div hlmSidebarWrapper>
+      <hlm-sidebar>
+        <div hlmSidebarHeader>{{ title() }}</div>
+        <div hlmSidebarContent>
+          <div hlmSidebarGroup>
+            <div hlmSidebarGroupLabel>Group 1</div>
+            <div hlmSidebarGroupContent>
+              <ul hlmSidebarMenu>
+                <li hlmSidebarMenuItem>
+                  <a hlmSidebarMenuButton>Item 1.1</a>
+                </li>
+                <li hlmSidebarMenuItem>
+                  <a hlmSidebarMenuButton>Item 1.2</a>
+                </li>
+                <li hlmSidebarMenuItem>
+                  <a hlmSidebarMenuButton>Item 1.3</a>
+                </li>
+                <li hlmSidebarMenuItem>
+                  <a hlmSidebarMenuButton>Item 1.4</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+            <div hlmSidebarGroup>
+            <div hlmSidebarGroupLabel>Group 2</div>
+            <div hlmSidebarGroupContent>
+              <ul hlmSidebarMenu>
+                <li hlmSidebarMenuItem>
+                  <a hlmSidebarMenuButton>Item 2.1</a>
+                </li>
+                <li hlmSidebarMenuItem>
+                  <a hlmSidebarMenuButton>Item 2.2</a>
+                </li>
+                <li hlmSidebarMenuItem>
+                  <a hlmSidebarMenuButton>Item 2.3</a>
+                </li>
+                <li hlmSidebarMenuItem>
+                  <a hlmSidebarMenuButton>Item 2.4</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div hlmSidebarFooter></div>
+      </hlm-sidebar>
+      <div>
+        <nav hlmBreadcrumb>
+          <ol hlmBreadcrumbList>
+            <li hlmBreadcrumbItem>
+              <a hlmBreadcrumbLink link="/">Home</a>
+            </li>
+            <li hlmBreadcrumbSeparator></li>
+            <li hlmBreadcrumbItem>
+              <hlm-breadcrumb-ellipsis />
+            </li>
+            <li hlmBreadcrumbSeparator></li>
+            <li hlmBreadcrumbItem>
+              <a hlmBreadcrumbLink link="/components">Components</a>
+            </li>
+            <li hlmBreadcrumbSeparator></li>
+            <li hlmBreadcrumbItem>
+              <span hlmBreadcrumbPage>Breadcrumb</span>
+            </li>
+          </ol>
+        </nav>
+        <p>{{ content() }}</p>
+      </div>
+    </div>
 
     <router-outlet />
   `,
-  styles: [],
 })
 export class App {
   protected readonly title = signal('Logix');
