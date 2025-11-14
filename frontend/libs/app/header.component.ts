7e9d99ff45ff5b4fc2ce1bfa799aaeac8d49e676
hekma <contact@hekma.dev>
2025-11-14 20:41:00 +0100

    feat(frontend): Add sidebar control button to header

--- frontend/libs/app/header.component.ts
+++ frontend/libs/app/header.component.ts
@@ -1,23 +1,31 @@
 import { Component, input } from "@angular/core";
 import { HlmBreadCrumbImports } from "@spartan-ng/helm/breadcrumb";
+import { HlmSidebarTrigger } from "@spartan-ng/helm/sidebar";
 
 @Component({
   selector: 'app-header',
-  imports: [HlmBreadCrumbImports],
+  imports: [HlmBreadCrumbImports, HlmSidebarTrigger],
   template: `
-    <nav hlmBreadcrumb>
-      <ol hlmBreadcrumbList>
-        @let breadcrumbs = value();
-        @for (item of breadcrumbs; let idx = $index; track idx) {
-          <li hlmBreadcrumbItem>
-            <a hlmBreadcrumbLink link="{{ item.url }}">{{ item.label }}</a>
-          </li>
-          @if (idx < breadcrumbs.length - 1) {
-            <li hlmBreadcrumbSeparator> </li>
-          }
-        }
-      </ol>
-    </nav>
+    <header>
+      <div class="flex items-center gap-2">
+        <button
+          hlmSidebarTrigger
+        ></button>
+        <nav hlmBreadcrumb>
+          <ol hlmBreadcrumbList>
+            @let breadcrumbs = value();
+            @for (item of breadcrumbs; let idx = $index; track idx) {
+              <li hlmBreadcrumbItem>
+                <a hlmBreadcrumbLink link="{{ item.url }}">{{ item.label }}</a>
+              </li>
+              @if (idx < breadcrumbs.length - 1) {
+                <li hlmBreadcrumbSeparator> </li>
+              }
+            }
+          </ol>
+        </nav>
+      </div>
+    </header>
   `
 })
 export class HeaderComponent {
