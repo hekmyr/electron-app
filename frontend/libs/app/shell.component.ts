2dc71b9e43b5f6d0b30d79ac3a505dd1c496fbe2
hekma <contact@hekma.dev>
2025-11-14 22:22:03 +0100

    feat(frontend): Added quick actions to header

--- frontend/libs/app/shell.component.ts
+++ frontend/libs/app/shell.component.ts
@@ -1,16 +1,20 @@
 import { Component, input } from "@angular/core";
-import { HeaderComponent, BreadcrumbItem } from "@libs/app/header.component";
+import { BreadcrumbItem, HeaderComponent, QuickAction } from "@libs/app/header.component";
 
 @Component({
   selector: 'app-shell',
   imports: [HeaderComponent],
   template: `
-    <div class="p-2">
-      <app-header [value]="breadcrumbs()" />
+    <div class="p-2 w-full">
+      <app-header
+        [value]="breadcrumbs()"
+        [actions]="actions()"
+      />
       <ng-content />
     </div>
   `
 })
 export class ShellComponent {
   public readonly breadcrumbs = input.required<BreadcrumbItem[]>();
+  public readonly actions = input<QuickAction[]>([]);
 }
--- frontend/src/pages/customers.page.ts
+++ frontend/src/pages/customers.page.ts
@@ -1,9 +1,18 @@
 import { DataService, DataServiceImpl } from "@/shared/services/data";
 import { Component, inject } from "@angular/core";
-import { BreadcrumbItem } from "@libs/app/header.component";
+import { BreadcrumbItem, IconValue, QuickAction } from "@libs/app/header.component";
 import { ShellComponent } from "@libs/app/shell.component";
 import { TableComponent } from '@libs/app/table.component';
+import { provideIcons } from "@ng-icons/core";
+import { lucidePlus } from "@ng-icons/lucide";
 import { CustomerDTO } from "@shared/dto/customer-dto.interface";
+
+const actions: QuickAction[] = [
+  {
+    icon: { name: 'lucide-plus', value: lucidePlus, key: 'lucidePlus' },
+    onClick: () => { }
+  }
+];
 
 @Component({
   selector: 'app-customers',
@@ -12,10 +21,16 @@ import { CustomerDTO } from "@shared/dto/customer-dto.interface";
     TableComponent
   ],
   template: `
-    <app-shell [breadcrumbs]="_breadcrumbs">
+    <app-shell
+      [breadcrumbs]="_breadcrumbs"
+      [actions]="actions"
+    >
       <app-table [data]="_customers" [columns]="_columns" />
     </app-shell>
-  `
+  `,
+  providers: [
+    provideIcons({ ...provideActionIcons(actions) })
+  ]
 })
 export class CustomersPage {
   private readonly _dataService: DataService;
@@ -23,6 +38,7 @@ export class CustomersPage {
   protected readonly _breadcrumbs: BreadcrumbItem[] = [
     { label: "Customers", url: "/customers" }
   ];
+  protected readonly actions = actions;
 
   protected readonly _customers: CustomerDTO[] = [];
   protected readonly _columns = [
@@ -74,4 +90,12 @@ export class CustomersPage {
     this._dataService = inject(DataServiceImpl);
     this._customers = this._dataService.customers.findCustomers(40);
   }
+}
+
+export function provideActionIcons(actions: QuickAction[]) {
+  let icons: Record<string, IconValue> = {};
+  for (const a of actions) {
+    icons[a.icon.key] = a.icon.value;
+  }
+  return icons;
 }
