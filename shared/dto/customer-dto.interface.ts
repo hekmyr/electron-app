e7f7e46f6291bf3805beb260da3302746f2eeea9
hekma <contact@hekma.dev>
2025-11-16 12:18:02 +0100

    refactor(frontend): Update customer dto interface

--- shared/dto/customer-dto.interface.ts
+++ shared/dto/customer-dto.interface.ts
@@ -1,7 +1,10 @@
 export interface CustomerDTO {
   id: string;
   email: string;
+  phone: string;
   firstName: string;
   lastName: string;
   birthdate: Date;
+  createdAt: Date;
+  updatedAt: Date;
 }
