5ec125ed486ea7eca723a9615b838ed888e1c703
hekma <contact@hekma.dev>
2025-11-21 19:34:55 +0100

    refactor(frontend): Enforce coding style
    - Use 2 spaces instead of 4 spaces for indentations - Remove function
    return types

--- frontend/src/shared/services/data/customer.service.ts
+++ frontend/src/shared/services/data/customer.service.ts
@@ -3,36 +3,36 @@ import { inject } from "@angular/core";
 import { CustomerDTO } from "@shared/dto/customer-dto.interface";
 
 export interface CustomerService {
-    findCustomers(limit: number): Map<string, CustomerDTO>;
-    updateCustomer(id: string, customer: CustomerDTO): void;
-    createCustomer(customer: CustomerDTO): void;
-    deleteCustomer(id: string): void;
+  findCustomers(limit: number): Map<string, CustomerDTO>;
+  updateCustomer(id: string, customer: CustomerDTO): void;
+  createCustomer(customer: CustomerDTO): void;
+  deleteCustomer(id: string): void;
 }
 
 export class CustomerServiceImpl implements CustomerService {
 
-    private readonly _mockService: MockService;
+  private readonly _mockService: MockService;
 
-    public constructor() {
-        this._mockService = inject(MockService);
-    }
+  public constructor() {
+    this._mockService = inject(MockService);
+  }
 
-    public findCustomers(limit: number): Map<string, CustomerDTO> {
-        const customers = this._mockService.findCustomers(limit);
-        const map = new Map<string, CustomerDTO>();
-        customers.forEach(customer => map.set(customer.id, customer));
-        return map;
-    }
+  public findCustomers(limit: number) {
+    const customers = this._mockService.findCustomers(limit);
+    const map = new Map<string, CustomerDTO>();
+    customers.forEach(customer => map.set(customer.id, customer));
+    return map;
+  }
 
-    public updateCustomer(id: string, customer: CustomerDTO): void {
-        // In a real app, this would update the backend
-    }
+  public updateCustomer(id: string, customer: CustomerDTO) {
+    // In a real app, this would update the backend
+  }
 
-    public createCustomer(customer: CustomerDTO): void {
-        // In a real app, this would create the customer in the backend
-    }
+  public createCustomer(customer: CustomerDTO) {
+    // In a real app, this would create the customer in the backend
+  }
 
-    public deleteCustomer(id: string): void {
-        // In a real app, this would delete from the backend
-    }
+  public deleteCustomer(id: string) {
+    // In a real app, this would delete from the backend
+  }
 }
