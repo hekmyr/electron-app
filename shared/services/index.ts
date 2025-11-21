57f7203c5e7dca319f6ea8ae06b713915539aafc
hekma <contact@hekma.dev>
2025-11-22 00:05:47 +0100

    refactor(shared): Added index.ts file to export shared services

---
+++ shared/services/index.ts
@@ -0,0 +1,4 @@
+export * from "./customer-service.interface";
+export * from "./electron-service.interface";
+export * from "./package-service.interface";
+export * from "./persistence-service.interface";
