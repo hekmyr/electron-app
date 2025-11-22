340e5a797ec081564cbeaa8560821a1d498d442d
hekma <contact@hekma.dev>
2025-11-23 00:11:18 +0100

    feat(desktop): Extend IPC for customers and packages

--- desktop/src/lib/ipc/customer-ipc.ts
+++ desktop/src/lib/ipc/customer-ipc.ts
@@ -1,10 +1,10 @@
-import { ipcMain } from "electron";
-import { CustomerRepository } from "../interfaces/repositories/customer-repository.interface";
-import Registrable from "../interfaces/registrable.interface";
-import { WithoutId } from "@/shared/helper";
 import { CustomerDTO } from "@/shared/dto/customer-dto.interface";
+import { WithoutId } from "@/shared/helper";
 import { PrismaClient } from "@prisma/client";
+import { ipcMain } from "electron";
 import { CustomerRepositoryImpl } from "../implementations/repositories/customer-repository-impl";
+import Registrable from "../interfaces/registrable.interface";
+import { CustomerRepository } from "../interfaces/repositories/customer-repository.interface";
 
 export default class CustomerIpc implements Registrable {
 
@@ -17,29 +17,46 @@ export default class CustomerIpc implements Registrable {
   }
 
   register() {
-    ipcMain.handle('customer:insert', async (_event, customer: WithoutId<CustomerDTO>) => {
+    ipcMain.handle('customer:insert', async (_event, customer: WithoutId<CustomerDTO>): Promise<string> => {
       return await this._customerRepositoryImpl.insert(customer);
     });
 
-    ipcMain.handle('customer:findById', async (_event, id: string) => {
+    ipcMain.handle('customer:findById', async (_event, id: string): Promise<CustomerDTO | null> => {
       const result = await this._customerRepositoryImpl.findById(id);
       if (!result) return null;
       return {
         id: result.id,
         email: result.email,
+        phone: result.phone,
         firstName: result.firstName,
         lastName: result.lastName,
-        birthdate: result.birthdate
-      };
+        birthdate: result.birthdate,
+        createdAt: result.createdAt,
+        updatedAt: result.updatedAt
+      } satisfies CustomerDTO;
     });
 
-    ipcMain.handle('customer:deleteById', async (_event, id: string) => {
+    ipcMain.handle('customer:findbyPage', async (_event, limit: number, page?: number): Promise<CustomerDTO[]> => {
+      const results = await this._customerRepositoryImpl.findbyPage(limit, page);
+      const customers: CustomerDTO[] = results.map(result => ({
+        id: result.id,
+        email: result.email,
+        phone: result.phone,
+        firstName: result.firstName,
+        lastName: result.lastName,
+        birthdate: result.birthdate,
+        createdAt: result.createdAt,
+        updatedAt: result.updatedAt
+      }));
+      return customers;
+    });
+
+    ipcMain.handle('customer:deleteById', async (_event, id: string): Promise<void> => {
       await this._customerRepositoryImpl.deleteById(id);
     });
 
-    ipcMain.handle('customer:updateById', async (_event, id: string, customer: CustomerDTO) => {
+    ipcMain.handle('customer:updateById', async (_event, id: string, customer: CustomerDTO): Promise<void> => {
       await this._customerRepositoryImpl.updateById(id, customer);
     });
-
   }
 }
