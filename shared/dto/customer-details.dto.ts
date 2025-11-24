b1d7e4c22f977c3372dd901dfb894e691a67ace8
hekma <contact@hekma.dev>
2025-11-24 20:58:07 +0100

    feat(shared): Added customer details dto

---
+++ shared/dto/customer-details.dto.ts
@@ -0,0 +1,11 @@
+import { AddressDTO } from "./address-dto.interface";
+import { CustomerDTO } from "./customer-dto.interface";
+import { DeliveryDTO } from "./delivery-dto.interface";
+import { PackageDTO } from "./package-dto.interface";
+
+export interface CustomerDetailsDTO {
+    customer: CustomerDTO;
+    packages: PackageDTO[];
+    addresses: AddressDTO[];
+    deliveries: DeliveryDTO[];
+}
