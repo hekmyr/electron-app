d32fbdfaf566c656594d3a230b33a7d2a1b39451
hekma <contact@hekma.dev>
2025-12-12 16:21:01 +0100

    refactor(frontend): Replace status input component in package for component

--- frontend/libs/package/package-form.component.ts
+++ frontend/libs/package/package-form.component.ts
@@ -1,13 +1,35 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, inject, input, output } from '@angular/core';
+import { Component, OnInit, inject, input, output, signal } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HlmInput } from '@libs/ui/input/src';
-import { isPackageStatus, PackageDTO } from '@shared/dto/package-dto.interface';
+import { isPackageStatus, packageStatus, PackageDTO, PackageStatus } from '@shared/dto/package-dto.interface';
+
+// Spartan UI Imports
+import { HlmButtonImports } from '@libs/ui/button/src';
+import { HlmCommandImports } from '@libs/ui/command/src';
+import { HlmIconImports } from '@libs/ui/icon/src';
+import { HlmPopoverImports } from '@libs/ui/popover/src';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideCheck, lucideChevronsUpDown, lucideSearch } from '@ng-icons/lucide';
+import { BrnCommandImports } from '@spartan-ng/brain/command';
+import { BrnPopoverImports } from '@spartan-ng/brain/popover';
 
 @Component({
   selector: 'package-form',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, HlmInput],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    HlmInput,
+    BrnCommandImports,
+    HlmCommandImports,
+    NgIcon,
+    HlmIconImports,
+    HlmButtonImports,
+    BrnPopoverImports,
+    HlmPopoverImports,
+  ],
+  providers: [provideIcons({ lucideChevronsUpDown, lucideSearch, lucideCheck })],
   template: `
     <form
       class="grid gap-4"
@@ -34,7 +56,49 @@ import { isPackageStatus, PackageDTO } from '@shared/dto/package-dto.interface';
 
         <label class="grid gap-2 text-sm">
           <span>Status</span>
-          <input hlmInput type="text" formControlName="status" />
+          @let statusComboboxState = _statusComboboxStateSignal();
+          @let selectedStatus = _selectedStatusSignal();
+          <brn-popover [state]="statusComboboxState" (stateChanged)="_statusComboboxStateSignal.set($event)" sideOffset="5">
+            <button
+              type="button"
+              class="w-full justify-between font-normal"
+              variant="outline"
+              brnPopoverTrigger
+              (click)="_statusComboboxStateSignal.set('open')"
+              hlmBtn
+            >
+              {{ selectedStatus ? _getStatusLabel(selectedStatus) : 'Select status...' }}
+              <ng-icon hlm size="sm" name="lucideChevronsUpDown" class="opacity-50" />
+            </button>
+            <hlm-command *brnPopoverContent="let ctx" hlmPopoverContent class="w-[200px] p-0">
+              <hlm-command-search>
+                <ng-icon hlm name="lucideSearch" />
+                <input placeholder="Search status..." hlm-command-search-input />
+              </hlm-command-search>
+              <div *brnCommandEmpty hlmCommandEmpty>No statuses found.</div>
+              <hlm-command-list>
+                <hlm-command-group>
+                  @for (status of _packageStatus; track status) {
+                    <button
+                      type="button" hlm-command-item
+                      class="font-normal"
+                      [value]="status"
+                      (selected)="selectStatus(status)"
+                    >
+                      <span>{{ _getStatusLabel(status) }}</span>
+                      <ng-icon
+                        hlm
+                        class="ml-auto"
+                        [class.opacity-0]="selectedStatus !== status"
+                        name="lucideCheck"
+                        hlmCommandIcon
+                      />
+                    </button>
+                  }
+                </hlm-command-group>
+              </hlm-command-list>
+            </hlm-command>
+          </brn-popover>
         </label>
       </div>
     </form>
@@ -53,9 +117,31 @@ export class PackageFormComponent implements OnInit {
     status: ['received', [Validators.required]],
   });
 
+  protected readonly _packageStatus = packageStatus;
+
+  protected readonly _statusComboboxStateSignal = signal<'closed' | 'open'>('closed');
+  protected readonly _selectedStatusSignal = signal<PackageStatus | undefined>(undefined);
+
+  private readonly _statusMap: Record<PackageStatus, string> = {
+    'received': 'Received',
+    'out_for_delivery': 'Out for delivery',
+    'delivered': 'Delivered',
+  };
+  protected _getStatusLabel(status: PackageStatus | undefined) {
+    if (!status) return '';
+    return this._statusMap[status] || status;
+  }
+
+  public selectStatus(status: PackageStatus) {
+    this._selectedStatusSignal.set(status);
+    this._statusComboboxStateSignal.set('closed');
+    this.form.patchValue({ status });
+  }
+
   public ngOnInit() {
     const pkg = this.packageSignal();
     if (pkg) {
+      this._selectedStatusSignal.set(pkg.status as PackageStatus);
       this.form.patchValue({
         name: pkg.name,
         description: pkg.description,
